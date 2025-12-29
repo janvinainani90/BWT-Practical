7ca646e5fb27ab32e520f4774827205ed39fe090
janvinainani90 <janvinainani90@gmail.com>
2025-12-29 19:48:13 +0530

    Create p7.6.js

---
+++ Practical 7/p7.6.js
@@ -0,0 +1,9 @@
+ function check(){
+            var age=prompt("Enter your age here: ");
+            if(age>=65){
+                alert("You are eligible for senior citizen discount.");
+            }
+            else{
+                alert("You are not eligible for senior citizen discount.");
+            }
+        }
