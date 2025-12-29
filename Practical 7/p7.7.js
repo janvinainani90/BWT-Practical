e0c7c9e74c2b82e7b0e8d4100956f3ad6c36006b
janvinainani90 <janvinainani90@gmail.com>
2025-12-29 19:49:32 +0530

    Create p7.7.js

---
+++ Practical 7/p7.7.js
@@ -0,0 +1,7 @@
+var x=prompt("Enter number here: ");
+        if(x>0){
+            alert("The number is positive.");
+        }
+        else if(x<0){alert("The number is negative.")}
+        else{alert("The number is zero.");
+            }
