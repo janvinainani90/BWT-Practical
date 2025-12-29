4c025aeb3ac99ff5795922885aa066ed7ea2c55b
janvinainani90 <janvinainani90@gmail.com>
2025-12-29 19:45:55 +0530

    Create p7.3.js

---
+++ Practical 7/p7.3.js
@@ -0,0 +1,8 @@
+    function show(){
+            var _num=14;
+            let strings="Hello User,";
+            const pi=3.14;
+            document.write(_num +" this is a number stored in var" + '<br>');
+            document.write(strings + "  this is a string stored in let" + '<br>');
+            document.write(pi +" this is a value of constant PI stored in const" + '<br>');
+        }
